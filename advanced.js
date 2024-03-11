3d59f03c4214f9b37ea34670caec6c84c765d064
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-11 15:00:26 +0000

    Finished advanced.js lab

--- advanced.js
+++ advanced.js
@@ -32,26 +32,32 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
-
-// console.log(england);
+unitedKingdom[1].touristAttractions = ["Big Ben"];
+england = unitedKingdom[1];
+console.log(england);
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = null;
-
-// console.log(wales);
+wales = unitedKingdom[2];
+wales.capital = "Cardiff"
+console.log(wales);
 
 /**
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
-
-// console.log(northernIrelandKeys);
+northernIrelandKeys = Object.keys(unitedKingdom[3]);
+console.log(northernIrelandKeys);
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the population of Wales, and say which is bigger.
  */
+
+if(unitedKingdom[0].population > wales.population){
+  console.log("Scotland has a bigger population.");
+}
+else{
+  console.log("Wales has a bigger population.");
+}
